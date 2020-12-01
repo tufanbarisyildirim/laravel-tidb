b474efc223230c951caea8e09f1a6f5ef6bab28b
Tufan Barış YILDIRIM <tufanbarisyildirim@gmail.com>
2020-12-01 21:38:30 +0300

    basic working DDL grammar

---
+++ src/TiDB/Grammar/TiDBQueryGrammar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TiDB\Grammar;
+
+use Illuminate\Database\Query\Grammars\MySqlGrammar;
+
+class TiDBQueryGrammar extends MySqlGrammar
+{
+    //just a place holder to help developers to have better compatibility
+    //for next versions.
+}
